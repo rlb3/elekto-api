3d7f5df19b91cf605d99b464aabc86dbf6e4faa2
Robert Boone <robert@rlb3.com>
2015-10-18 15:18:19 -0500

    Create and validate election

--- app/models/election.rb
+++ app/models/election.rb
@@ -1,3 +1,6 @@
 class Election < ActiveRecord::Base
   belongs_to :owner
+  validates :name, presence: true, uniqueness: true
+  validates :start_at, presence: true, uniqueness: true
+  validates :end_at, presence: true, uniqueness: true
 end
