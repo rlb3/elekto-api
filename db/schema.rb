7b4111d49307db8b7444e1453a63b14bac4703cd
Robert Boone <robert@rlb3.com>
2015-11-22 09:43:33 -0600

    Add image to candidate model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151122151827) do
+ActiveRecord::Schema.define(version: 20151122153647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,12 @@ ActiveRecord::Schema.define(version: 20151122151827) do
     t.string   "last_name"
     t.text     "bio"
     t.integer  "election_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "elections", force: :cascade do |t|
--- spec/models/candidate_spec.rb
+++ spec/models/candidate_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Candidate, type: :model do
   it { is_expected.to validate_presence_of(:last_name) }
   it { is_expected.to validate_presence_of(:bio) }
   it { is_expected.to belong_to :election }
+  it { is_expected.to have_attached_file(:image) }
+  it { is_expected.to validate_attachment_content_type(:image).
+                       allowing('image/png', 'image/gif').
+                       rejecting('text/plain', 'text/xml')
+  }
 end
