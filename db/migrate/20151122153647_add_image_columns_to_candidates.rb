7db40a54f13db7d5d19b10a0e31b4009ef1bf0aa
Robert Boone <robert@rlb3.com>
2015-11-22 09:51:54 -0600

    Copy paste mistake

--- db/migrate/20151122153647_add_image_columns_to_candidates.rb
+++ db/migrate/20151122153647_add_image_columns_to_candidates.rb
@@ -1,7 +1,7 @@
 class AddImageColumnsToCandidates < ActiveRecord::Migration
   def change
     reversible do |dir|
-      change_table :products do |t|
+      change_table :candidates do |t|
         dir.up   { add_attachment :candidates, :image }
         dir.down { remove_attachment :candidates, :image }
       end
